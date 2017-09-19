a769c7db1b08b8a8ab2aecba71911bac779814df
D7703_14 <d7703_14@D7703-14ui-iMac.local>
2017-09-19 15:55:40 +0900

    new

--- mapViewPList/ViewController.swift
+++ mapViewPList/ViewController.swift
@@ -7,17 +7,110 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController ,MKMapViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var minMap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var annotations = [MKPointAnnotation]()
+        zoomToRegion()
+        
+        let path = Bundle.main.path(forResource: "ViewPoint2", ofType: "plist")
+       //파일 경로 찾아서 오기
+        
+        let contents = NSArray(contentsOfFile: path!)
+       
+        
+        if let myItems = contents{
+            for item in myItems{
+                let lat = (item as AnyObject).value(forKey: "lat")
+                let long = (item as AnyObject).value(forKey: "long")
+                let title = (item as AnyObject).value(forKey: "title")
+                let subTitle = (item as AnyObject).value(forKey: "subTitle")
+                
+               
+                
+                let myLat = (lat as! NSString).doubleValue
+                let myLong = (long as! NSString).doubleValue
+                let myTitle = title as! String
+                let mySubTitle = subTitle as! String
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate.latitude = myLat
+                annotation.coordinate.longitude = myLong
+                annotation.title = myTitle
+                annotation.subtitle = mySubTitle
+                
+                annotations.append(annotation)
+                
+
+            }
+        } else {
+            
+            
+        }
+        minMap.showAnnotations(annotations, animated: true)
+        minMap.addAnnotations(annotations)
     }
 
+    func zoomToRegion(){
+        let center = CLLocationCoordinate2D(latitude: 35.166197, longitude: 129.072594)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        
+        minMap.setRegion(region, animated: true)
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        var  annotationView = minMap.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+
+            
+            if annotation.title! == "부산시민공원" {
+                // 부시민공원
+                annotationView?.pinTintColor = UIColor.green
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 53, height: 53))
+                leftIconView.image = UIImage(named:"citizen_logo.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+                
+            } else if annotation.title! == "DIT 동의과학대학교" {
+                // 동의과학대학교
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"DIT_logo.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+                
+            } else {
+                // 송상현광장
+                annotationView?.pinTintColor = UIColor.blue
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"Songsang.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+            }
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        let btn = UIButton(type: .detailDisclosure)
+        annotationView?.rightCalloutAccessoryView = btn
+        
+        return annotationView
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
